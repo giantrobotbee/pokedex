b7553a7f55f3247cdcacfc9325060b2cab2601d9
Avery Barron <giantrobotbee@Cassiopeia.local>
2020-02-08 17:30:28 -0800

    got pagination with URL states working

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,14 @@
+import { Fragment } from "react";
 import useSWR from "swr";
 import * as R from "ramda";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
-const API_URL = "https://pokeapi.co/api/v2/";
+import styles from "./app.module.css";
+
+const API_URL = "https://pokeapi.co/api/v2";
+const PAGE_LIMIT = 10;
 
 async function fetcher(url) {
   const res = await fetch(url);
@@ -11,32 +16,66 @@ async function fetcher(url) {
   return json;
 }
 
-const renderPokemon = mon => {
+const Mon = ({ mon }) => {
   return (
-    <li key={mon.name}>
+    <li>
       <span>{mon.name}</span>
     </li>
   );
 };
 
+const Dex = ({ pokemon }) => {
+  return (
+    <Fragment>
+      <ul>
+        {R.map(
+          mon => (
+            <Mon mon={mon} key={mon.name} />
+          ),
+          pokemon
+        )}
+      </ul>
+    </Fragment>
+  );
+};
+
 function HomePage() {
-  const [path, changePath] = useState(API_URL + "pokemon");
+  const router = useRouter();
+  const { pageNum } = router.query;
+  const path = `${API_URL}/pokemon?limit=${PAGE_LIMIT}&offset=${pageNum *
+    PAGE_LIMIT -
+    10}`;
   const { data, error } = useSWR(path, fetcher);
 
-  const pokemon = data && data.results;
+  const howManyPokemon = data ? data.count : 0;
+  const pageCount = Math.ceil(howManyPokemon / PAGE_LIMIT);
 
-  const nextPage = () => {
-    changePath(data.next);
+  const mainSection = () => {
+    if (error) return <div>Fuck...</div>;
+    if (!data) return <div>Big Loamd...</div>;
+    if (data.results.length === 0) return <div>I got nothin' for ya...</div>;
+
+    return (
+      <Fragment key={pageNum}>
+        <Dex pokemon={data.results} />
+        <div className={styles.pageNav}>
+          <Link href={`?pageNum=${Number(pageNum) - 1}`}>
+            <button disabled={!Boolean(data.previous) ?? "disabled"}>
+              Previous
+            </button>
+          </Link>
+          <span>{`Page ${Number(pageNum)} of ${pageCount}`}</span>
+          <Link href={`?pageNum=${Number(pageNum) + 1}`}>
+            <button disabled={Number(pageNum) === pageCount && "disabled"}>
+              Next
+            </button>
+          </Link>
+        </div>
+      </Fragment>
+    );
   };
 
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
-  return (
-    <div>
-      <ul>{R.map(renderPokemon, pokemon)}</ul>
-      <button onClick={nextPage}>Next</button>
-    </div>
-  );
+  return <div className={styles.pokedex}>{mainSection()}</div>;
 }
 
 export default HomePage;
